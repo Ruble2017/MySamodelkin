abc15119e626f70f4b7e94cddd1d8fa140d4c547
Ruble2017 <truemaster@ya.ru>
2021-01-24 22:49:15 +0700

    before corutine

--- app/src/main/java/com/tomsk/android/mysamodelkin/NewCharacterActivity.kt
+++ app/src/main/java/com/tomsk/android/mysamodelkin/NewCharacterActivity.kt
@@ -7,23 +7,31 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_new_character.*
 private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
 
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
 class NewCharacterActivity : AppCompatActivity() {
     private var characterData = CharacterGenerator.generate()
 
     // сохранение состояния
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        //savedInstanceState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        savedInstanceState.characterData= characterData
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
 
-        characterData = savedInstanceState?.let {
-            it.getSerializable(CHARACTER_DATA_KEY)
-                    as CharacterGenerator.CharacterData
-        } ?: CharacterGenerator.generate()
+//        characterData = savedInstanceState?.let {
+//            it.getSerializable(CHARACTER_DATA_KEY)
+//                    as CharacterGenerator.CharacterData
+//        } ?: CharacterGenerator.generate()
+
+        characterData = savedInstanceState?.characterData ?:
+                CharacterGenerator.generate()
 
         generateButton.setOnClickListener {
             characterData = CharacterGenerator.generate()
